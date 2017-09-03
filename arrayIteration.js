201f4e5ee18d8ded113790c878e215bf94cbee57
duongvn <ddvnguyen92@gmail.com>
2017-09-03 15:28:08 -0600

    generate new function mostCostEffective

--- arrayIteration.js
+++ arrayIteration.js
@@ -1,3 +1,4 @@
+var costs  = [.25, .27, .25, .25, .25, .25, .33, .31, .25, .29, .27, .22, .31, .25, .25, .33, .21, .25, .25, .25, .28, .25, .24, .22, .20, .25, .30, .25, .24, .25, .25, .25, .27, .26, .29];
 var scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69, 34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];
 
 function generateHighScore(scores){
@@ -16,7 +17,7 @@ function generateHighScore(scores){
 }
 
 // after finding higher scores, we can now push them to a new array index
-function getBestResult(highestScore, scores){
+function getBestResult(scores, highestScore){
 	var bestSolution = []; //generating empty array
 	//scanning through 'scores' array finding highest score
 	for(var i = 0; i < scores.length; i++){
@@ -31,4 +32,22 @@ console.log("Bubbles tests: " + scores.length);
 console.log("Highest bubble score: " + highestScore); 
 
 var bestSolution = getBestResult(scores, highestScore); //assign returned values into 'bestSolution'
-console.log("Solution with the highest score: " + bestSolution);
+console.log("Solutions with the highest score: " + bestSolution);
+
+function getMostEffective (scores, costs, highestScore){
+	var cost = 100; //starts high, but lower everytime we find a lower solution
+	var index; //index of lowest cost solution
+	
+	for (var i = 0; i < scores.length; i++){
+		if (scores[i] == highestScore){ //check if the score has the high score
+			if(cost > costs[i]){ //if current cost is > than solution costs
+				index = i; //keeping track of what solution it is
+				cost = costs[i]; //store as the lowest cost
+			}
+		}
+	}
+	return index;
+}
+var mostCostEffective = getMostEffective(scores, costs, highestScore);
+//mostCostEffective contains the index(Bubble Solution #)
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
